932c7a4a7c8fa67eb3c60ba2fc73780a459a55ff
christianlacuesta <lacuestachristian@yahoo.com>
2022-07-26 09:07:49 +0300

    defined model dependencies in admin table

--- controllers/table/admin-table/admin-table.js
+++ controllers/table/admin-table/admin-table.js
@@ -5,7 +5,13 @@ const Configs = require('../../../models/admin-models/config-model/configs');
  * Below is the list of Admin Table Models. *
  *******************************************/
 
-
+const Group = require('../../../models/admin-models/group-model/group');
+const Interface = require('../../../models/admin-models/interface-model/interface');
+const Item = require('../../../models/admin-models/item-model/item');
+const Object = require('../../../models/admin-models/object-model/object');
+const Organization = require('../../../models/admin-models/organization-model/organization');
+const System = require('../../../models/admin-models/system-model/system');
+const User = require('../../../models/admin-models/user-model/user');
 
 const table = {
     tableName: null,
