acec26f37c82f3a98595f2110ac43f26313301dd
christianlacuesta <lacuestachristian@yahoo.com>
2022-07-28 12:09:34 +0300

    added validated function

--- controllers/form/workflow-form/workflow-form.js
+++ controllers/form/workflow-form/workflow-form.js
@@ -3,13 +3,21 @@ const Items = require('../../../models/item-models/item');
 const Objects = require('../../../models/object-models/object');
 
 const form = {
+    organizationId: tableParams.organizationId, 
+    systemId: tableParams.systemId,
+    interfaceId: tableParams.interfaceId,
     step: null,
-    items: null
+    items: null,
+    objects: null
 }
 
 
 exports.getWorkflowForm = async(req, res, next) => {
   
+}
+
+const validateParameters = (tableParams) => {
+    
 }
 
 const getSteps = () => {
