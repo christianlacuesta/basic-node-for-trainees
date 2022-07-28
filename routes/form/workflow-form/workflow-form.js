949e5f52e1a5403c3f0ef7bf577028a061f00278
christianlacuesta <lacuestachristian@yahoo.com>
2022-07-28 12:48:25 +0300

    updated workflow form routes

--- routes/form/workflow-form/workflow-form.js
+++ routes/form/workflow-form/workflow-form.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const workflowFormController = require('../../../controllers/form/workflow-form/workflow-form');
 
-router.post('/workflowform/workflowform', [], workflowFormController.getWorkflowForm);
+router.post('/workflowform/getworkflowform', [], workflowFormController.getWorkflowForm);
 
 module.exports = router;
